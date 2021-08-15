edafdb4eefeaadc06c1a1720ca5d1c58f6108fd3
lsc58461 <79037820+lsc58461@users.noreply.github.com>
2021-08-16 00:39:39 +0900

    Update Crawling_Covid.py

--- Crawling_Covid.py
+++ Crawling_Covid.py
@@ -1,8 +1,6 @@
 from bs4 import BeautifulSoup
 import urllib.request
 
-
-
 url = 'http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=1&brdGubun=11&ncvContSeq=&contSeq=&board_id=&gubun='
 html = urllib.request.urlopen(url)
 soup = BeautifulSoup(html, "html.parser")
