46e7ba18d23f2a822f4904b42a1332efe052be5e
lsc58461 <79037820+lsc58461@users.noreply.github.com>
2021-10-23 19:53:02 +0900

    Update nixcovidbot.py

--- nixcovidbot.py
+++ nixcovidbot.py
@@ -1,23 +1,69 @@
 import os
-from discord.ext import commands
-from discord_components import DiscordComponents, Button, ButtonStyle, InteractionType, component
+import discord
+from Crawling_Covid import datecr, totalcovid, todaytotalcovid, todaydomecovid, todayforecovid, totalca, todayca, totalcaing, todaycaing, totaldead, todaydead
+from discord_components import DiscordComponents, Button, ButtonStyle, Select, Interaction, SelectOption
 
-bot = commands.Bot(command_prefix = "C!")
-Token = os.environ["Token"]
-startup_extensions = ['cogs.Button_Covid']
-os.chdir('/app/cogs')
+client = discord.Client()
+DiscordComponents(client)
 
-if __name__ == "__main__":
-    for extension in startup_extensions:
-        try:
-            bot.load_extension(extension)
-        except Exception as e:
-            exc = '{} : {}'.format(type(e).__name__, e)
-            print('파일 로딩 실패 {}\n{}'.format(extension,exc))
+Token = os.environ['Token']
 
-@bot.event
+@client.event
+async def on_message(msg):
+	if msg.content == "!버튼":
+            try:
+                await msg.delete()
+            except:
+                pass
+            await msg.channel.send(embed=discord.Embed(color=0xFF0F13, title="코로나19 국내 발생현황", description="", url='http://ncov.mohw.go.kr/').set_footer(text="밑에 버튼을 눌러 알아보세요!"), components=[[Button(label="🦠 확진환자", id="Confirmed_case", style=ButtonStyle.blue), Button(label="😷 격리중", id="Quarantine", style=ButtonStyle.blue), Button(label="😂 격리해제", id="Quarantine_release", style=ButtonStyle.blue), Button(label="🩸 사망자", id="Death", style=ButtonStyle.red)]])
+
+@client.event
+async def on_button_click(interaction: Interaction):
+	try:
+		if interaction.component.custom_id == "Confirmed_case":
+			_datecr = datecr()
+			_datecr = _datecr.string
+			_totalcovid = totalcovid()
+			_todaytotalcovid = todaytotalcovid()
+			_todaydomecovid = todaydomecovid()
+			_todayforecovid = todayforecovid()
+			embed = discord.Embed(title='코로나19 국내 발생현황',description="",color=0xFF0F13).add_field(name='🦠 확진환자',value=f'{_totalcovid}({_todaytotalcovid}) 명'f'\n\n국내발생: {_todaydomecovid} 명'f'\n해외유입: {_todayforecovid} 명',inline=False).set_footer(text=_datecr)
+			await interaction.respond(embed=embed)
+	except:
+		await interaction.respond("BT_1 Error : 오류가 계속 될 시 문의 바랍니다.")
+	try:
+		if interaction.component.custom_id == "Quarantine":
+			_datecr = datecr()
+			_datecr = _datecr.string
+			_totalcaing = totalcaing()
+			_todaycaing = todaycaing()
+			embed = discord.Embed(title='코로나19 국내 발생현황',description="",color=0xFF0F13,).add_field(name='😷 격리중',value=f'{_totalcaing}({_todaycaing}) 명',inline=False).set_footer(text=_datecr)
+			await interaction.respond(embed=embed)
+	except:
+		await interaction.respond("BT_2 Error : 오류가 계속 될 시 문의 바랍니다.")
+	try:
+		if interaction.component.custom_id == "Quarantine_release":
+			_datecr = datecr()
+			_datecr = _datecr.string
+			_totalca = totalca()
+			_todayca = todayca()
+			embed = discord.Embed(title='코로나19 국내 발생현황',description="",color=0xFF0F13).add_field(name='😂 격리해제',value=f'{_totalca}({_todayca}) 명',inline=False).set_footer(text=_datecr)
+			await interaction.respond(embed=embed)
+	except:
+		await interaction.respond("BT_3 Error : 오류가 계속 될 시 문의 바랍니다.")
+	try:
+		if interaction.component.custom_id == "Death":
+			_datecr = datecr()
+			_datecr = _datecr.string
+			_totaldead = totaldead()
+			_todaydead = todaydead()
+			embed = discord.Embed(title='코로나19 국내 발생현황',description="",color=0xFF0F13).add_field(name='🩸 사망자',value=f'{_totaldead}({_todaydead}) 명',inline=False).set_footer(text=_datecr)
+			await interaction.respond(embed=embed)
+	except:
+		await interaction.respond("BT_4 Error : 오류가 계속 될 시 문의 바랍니다.")
+    		
+@client.event
 async def on_ready():
-    DiscordComponents(bot, change_discord_methods=True)
-    print("Ready")
+	print("ready")
 
-bot.run(Token)
+client.run(Token)
