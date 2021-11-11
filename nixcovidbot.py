6e5a6b6a8206ea3d6143c3f3d5ca21c5a96fe973
lsc58461 <79037820+lsc58461@users.noreply.github.com>
2021-11-11 19:31:24 +0900

    Update nixcovidbot.py

--- nixcovidbot.py
+++ nixcovidbot.py
@@ -1,6 +1,6 @@
 import os
 import discord
-from Crawling_Covid import datecr, Total_Infection, Today_Infection, Vaccination_Status, Total_Death, Today_Death
+from Crawling_Covid import time, datecr, Total_Infection, Today_Infection, Vaccination_Status, Total_Death, Today_Death
 from discord_components import DiscordComponents, Button, ButtonStyle, Select, Interaction, SelectOption
 
 client = discord.Client()
@@ -28,7 +28,8 @@ async def on_button_click(interaction: Interaction):
 			_datecr = _datecr.string
 			_Total_Infection = Total_Infection()
 			_Today_Infection = Today_Infection()
-			embed = discord.Embed(title='🦠 확진환자',description="",color=0x368AFF).add_field(name='누적 확진자',value=f'{_Total_Infection} 명',inline=False).add_field(name='당일 확진자',value=f'{_Today_Infection} 명',inline=False).set_footer(text=_datecr)
+			embed = discord.Embed(title='🦠 확진환자',description="",color=0x368AFF).add_field(name='누적 확진자',value=f'{_Total_Infection} 명',inline=False).add_field(name='신규 확진자',value=f'{_Today_Infection} 명',inline=False).set_footer(text=_datecr)
+			print(f'{time()})  요청자: {interaction.user.name}\n')
 			await interaction.respond(embed=embed)
 	except:
 		embed = discord.Embed(title='BT_1 Error',description="잠시 후 다시 시도해주세요\n오류가 계속 될 시 문의 바랍니다.",color=0xFF0F13)
@@ -36,11 +37,8 @@ async def on_button_click(interaction: Interaction):
 	try:
 		if interaction.component.custom_id == "Death":
 			await interaction.respond(type=5)
-			_datecr = datecr()
-			_datecr = _datecr.string
-			_Total_Death = Total_Death()
-			_Today_Death = Today_Death()
-			embed = discord.Embed(title='🩸 사망자',description="",color=0xFF0F13).add_field(name='누적 사망자',value=f'{_Total_Death} 명',inline=False).add_field(name='당일 사망자',value=f'{_Today_Death} 명',inline=False).set_footer(text=_datecr)
+			embed = discord.Embed(title='🩸 사망자',description="",color=0xFF0F13).add_field(name='누적 사망자',value=f'{Total_Death()} 명',inline=False).add_field(name='당일 사망자',value=f'{Today_Death()} 명',inline=False).set_footer(text=datecr())
+			print(f'{time()})  요청자: {interaction.user.name}\n')
 			await interaction.respond(embed=embed)
 	except:
 		embed = discord.Embed(title='BT_2 Error',description="잠시 후 다시 시도해주세요\n오류가 계속 될 시 문의 바랍니다.",color=0xFF0F13)
@@ -48,10 +46,9 @@ async def on_button_click(interaction: Interaction):
 	try:
 		if interaction.component.custom_id == "Vaccination_Status":
 			await interaction.respond(type=5)
-			_datecr = datecr()
-			_datecr = _datecr.string
 			_Vaccination_Status = Vaccination_Status()
-			embed = discord.Embed(title='💉 예방접종현황',description="",color=0x1DDB16,).add_field(name='1차 접종',value=f'{_Vaccination_Status[0]}',inline=True).add_field(name='누적 1차 접종',value=f'{_Vaccination_Status[2]} 명',inline=True).add_field(name='신규 1차 접종',value=f'{_Vaccination_Status[3]} 명',inline=True).add_field(name='접종 완료',value=f'{_Vaccination_Status[1]} 명',inline=True).add_field(name='누적 접종 완료',value=f'{_Vaccination_Status[4]} 명',inline=True).add_field(name='신규 접종 완료',value=f'{_Vaccination_Status[5]} 명',inline=True).set_footer(text=_datecr)
+			embed = discord.Embed(title='💉 예방접종현황',description="",color=0x1DDB16,).add_field(name='1차 접종',value=f'{_Vaccination_Status[0]}',inline=True).add_field(name='누적 1차 접종',value=f'{_Vaccination_Status[2]} 명',inline=True).add_field(name='신규 1차 접종',value=f'{_Vaccination_Status[3]} 명',inline=True).add_field(name='접종 완료',value=f'{_Vaccination_Status[1]} 명',inline=True).add_field(name='누적 접종 완료',value=f'{_Vaccination_Status[4]} 명',inline=True).add_field(name='신규 접종 완료',value=f'{_Vaccination_Status[5]} 명',inline=True).set_footer(text=datecr())
+			print(f'{time()})  요청자: {interaction.user.name}\n')
 			await interaction.respond(embed=embed)
 	except:
 		embed = discord.Embed(title='BT_3 Error',description="잠시 후 다시 시도해주세요\n오류가 계속 될 시 문의 바랍니다.",color=0xFF0F13)
